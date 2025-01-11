a3972cb5f254ceb8a3656959cffe6c2550782e04
Guilherme Maciel <guilhermemcl1503@gmail.com>
2025-01-11 11:08:03 -0300

    adicionado rotas para listar, criar e obter transações; atualizado README com requisitos

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,26 +5,43 @@ import { randomUUID } from "node:crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
+  app.get ('/summary', async (request, reply) => {
+    const sumary = await knex('transactions').sum('amount' ,{ as : 'amount'}).first()
 
-  app.post('/', async (request , reply ) => {
-    //{ title , amount , type : 'credit' | 'debit' }
+    return { sumary }
+  })
 
+
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*')
+    return { transactions }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionByIdParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = getTransactionByIdParamsSchema.parse(request.params)
+
+    const trasaction = await knex('transactions').select('*').where('id', id).first()
+    return { trasaction }
+
+  })
+
+  app.post('/', async (request, reply) => {
+    //{ title , amount , type : 'credit' | 'debit'    }
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit'])
     })
-
     const { title, amount, type } = createTransactionBodySchema.parse(request.body)
-
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
     })
-
     return reply.status(201).send()
-
-
   })
 }
